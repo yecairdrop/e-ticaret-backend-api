bb69ea7aab9d5a577a4f054dae61977d06f43718
yecairdrop <yakupcelebiefe@gmail.com>
2026-07-30 10:31:39 +0300

    feat: Sepet güvenliği, sipariş ve stok düşme mantığı ile admin sipariş yönetimi eklendi

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 const { addToCart, getCart, updateCartItem, removeCartItem } = require('../controllers/cartController');
 
-router.get('/:user_id', getCart);
-router.post('/', addToCart);
-router.put('/:id', updateCartItem);
-router.delete('/:id', removeCartItem);
+// Kapıdaki güvenlik görevlimizi (authMiddleware) içeri alıyoruz
+const verifyToken = require('../middleware/authMiddleware');
+
+// Rotaların arasına verifyToken ekliyoruz
+router.get('/:user_id', verifyToken, getCart);
+router.post('/', verifyToken, addToCart);
+router.put('/:id', verifyToken, updateCartItem);
+router.delete('/:id', verifyToken, removeCartItem);
 
 module.exports = router;
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, getUserOrders } = require('../controllers/orderController');
+const { createOrder, getUserOrders, getAllOrders, updateOrderStatus } = require('../controllers/orderController');
 
-// Kullanıcının sipariş geçmişini getir
-router.get('/:user_id', getUserOrders);
+// Güvenlik görevlilerimiz
+const verifyToken = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
 
-// Sepeti siparişe dönüştür
-router.post('/', createOrder);
+// 1. Müşteri Rotaları (Sadece giriş yapmış olması yeterli)
+router.post('/', verifyToken, createOrder);
+router.get('/kullanici/:user_id', verifyToken, getUserOrders);
+
+// 2. ADMİN Rotaları (Hem giriş yapacak hem de rolü 'admin' olacak)
+router.get('/admin/all', verifyToken, adminMiddleware, getAllOrders);
+router.put('/admin/:id/status', verifyToken, adminMiddleware, updateOrderStatus);
 
 module.exports = router;
